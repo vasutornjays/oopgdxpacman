434f32abff70fa9c8c58088da6993b676e04f6d4
vasutorn siriyakorn <vasutornj@gmail.com>
2016-10-08 19:51:21 +0700

    init project

---
+++ desktop/src/com/jay/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jay.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jay.mygdx.game.PacmanGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PacmanGame(), config);
+	}
+}
